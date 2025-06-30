aa93aca4bf32d766128fbbf5c1e654af71773a52
damar <damar@gitlab.lab>
2025-06-30 13:04:01 +0700

    fix layout issues in DashboardLayout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,7 +14,6 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { cn } from "@/lib/utils"
-import { SessionProvider } from "next-auth/react"
 
 export default function DashboardLayout({
   children,
@@ -25,7 +24,7 @@ export default function DashboardLayout({
       )}>
         <SidebarProvider>
           <AppSidebar />
-          <SidebarInset>
+          <SidebarInset className="h-[95vh]">
             <header className="flex h-16 shrink-0 items-center gap-2">
               <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
@@ -50,7 +49,8 @@ export default function DashboardLayout({
             </header>
             <div className={cn(
               "flex flex-1 flex-col gap-4 p-4 pt-0",
-              "bg-gray-100 dark:bg-gray-700"
+              "bg-gray-100 dark:bg-gray-700",
+              "overflow-y-auto"
               )}>
                  { children }
             </div>
