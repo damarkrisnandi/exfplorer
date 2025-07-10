bb1f12bf2ccccdb7048372359dacd0c69a02e6cd
damar <damar@gitlab.lab>
2025-07-10 21:25:06 +0700

    card scatter-plot

--- src/components/app-scatter-plot.tsx
+++ src/components/app-scatter-plot.tsx
@@ -4,6 +4,7 @@ import type { Element } from "@/lib/bootstrap-type"
 import { api } from "@/trpc/react"
 import { Ellipsis, X } from "lucide-react"
 import { LabelList, Legend, ResponsiveContainer, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from "recharts"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
 
 type AxisData = {
   dataKey: string,
@@ -23,10 +24,10 @@ type AppScatterPlotProps = {
 }
 function AppScatterPlot({ className, dataX, dataY }: AppScatterPlotProps) {
   const dataSeparation = [
-      { label: "FWD",  filter: (el: Element) => el.element_type == 4 && el.minutes > 90 && Number(el.points_per_game) > 5, fill: '#8884d8' },
-      { label: "MID", filter: (el:Element) => el.element_type == 3 && el.minutes > 90 && Number(el.points_per_game) > 5, fill: '#82ca9d'},
-      { label: "DEF",  filter: (el: Element) => el.element_type == 2 && el.minutes > 90 && Number(el.points_per_game) > 3, fill: '#ff5b00' },
-      { label: "GKP", filter: (el:Element) => el.element_type == 1 && el.minutes > 90 && Number(el.points_per_game) > 2, fill: '#ff00ff'}
+    { label: "FWD", filter: (el: Element) => el.element_type == 4 && el.minutes > 90 && Number(el.points_per_game) > 5, fill: '#8884d8' },
+    { label: "MID", filter: (el: Element) => el.element_type == 3 && el.minutes > 90 && Number(el.points_per_game) > 5, fill: '#82ca9d' },
+    { label: "DEF", filter: (el: Element) => el.element_type == 2 && el.minutes > 90 && Number(el.points_per_game) > 3, fill: '#ff5b00' },
+    { label: "GKP", filter: (el: Element) => el.element_type == 1 && el.minutes > 90 && Number(el.points_per_game) > 2, fill: '#ff00ff' }
   ]
   const { data: bootstrap, isLoading, isError } = api.bootstrap.get.useQuery();
 
@@ -37,30 +38,39 @@ function AppScatterPlot({ className, dataX, dataY }: AppScatterPlotProps) {
 
 
   return (
-    <ResponsiveContainer height="100%" width="100%" minHeight={300} minWidth={100} aspect={1.0 / 1.0}>
-      <ScatterChart
-        width={400}
-        height={400}
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 10,
-          left: 10,
-        }}
-      >
-        {/* <CartesianGrid strokeDasharray="3 3" /> */}
-        <XAxis {...dataX}  domain={[0, 12]} />
-        <YAxis {...dataY}  domain={[0, 12]} />
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Pts/game vs XP_o5_current</CardTitle>
+        <CardDescription>Point per game versus XPoint last 5 games</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer height="100%" width="100%" minHeight={300} minWidth={100} aspect={1.0 / 1.0}>
+          <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 10,
+              left: 10,
+            }}
+          >
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <XAxis {...dataX} domain={[0, 12]} />
+            <YAxis {...dataY} width={20} domain={[0, 12]} />
 
-        {/* <Tooltip cursor={{ strokeDasharray: "3 3" }} /> */}
-        <Legend />
-        {dataSeparation.map((obj: { label: string, filter: (el: Element) => boolean, fill: string}) => (
-          <Scatter key={obj.label} name={obj.label} data={bootstrap.elements.filter((el: Element) => obj.filter(el))} fill={obj.fill}>
-            <LabelList dataKey="web_name" position="left" />
-          </Scatter>
-        ))}
-      </ScatterChart>
-    </ResponsiveContainer>
+            {/* <Tooltip cursor={{ strokeDasharray: "3 3" }} /> */}
+            <Legend />
+            {dataSeparation.map((obj: { label: string, filter: (el: Element) => boolean, fill: string }) => (
+              <Scatter key={obj.label} name={obj.label} data={bootstrap.elements.filter((el: Element) => obj.filter(el))} fill={obj.fill}>
+                <LabelList dataKey="web_name" position="left" />
+              </Scatter>
+            ))}
+          </ScatterChart>
+        </ResponsiveContainer>
+
+      </CardContent>
+    </Card>
   );
 }
 
